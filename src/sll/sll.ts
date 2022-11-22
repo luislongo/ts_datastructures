cc6490a9f1700ad79efa04692bf79a190d20451c
luislongo <luis.longof@gmail.com>
2022-11-22 13:09:21 -0300

    [SLL] Adds insertion and removal methods

--- src/sll/sll.ts
+++ src/sll/sll.ts
@@ -11,4 +11,53 @@ export class SLL {
   root: SLLNode | undefined;
 
   constructor() {}
+
+  push(value: number) {
+    const node = new SLLNode(value);
+
+    if (!this.root) {
+      this.root = node;
+      return;
+    }
+
+    let current = this.root;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  pop() {
+    if (!this.root) {
+      return;
+    }
+
+    let current = this.root;
+    let prev = current;
+    while (current.next) {
+      prev = current;
+      current = current.next;
+    }
+    prev.next = undefined;
+  }
+
+  shift() {
+    if (!this.root) {
+      return;
+    }
+
+    this.root = this.root.next;
+  }
+
+  unshift(value: number) {
+    const node = new SLLNode(value);
+
+    if (!this.root) {
+      this.root = node;
+      return;
+    }
+
+    node.next = this.root;
+    this.root = node;
+  }
 }
