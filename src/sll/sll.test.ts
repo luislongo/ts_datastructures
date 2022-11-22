34da22efbefd8c2c40f4e82c5b45e15583a0a388
luislongo <luis.longof@gmail.com>
2022-11-22 13:33:52 -0300

    [SLL] Refactors method return types

--- src/sll/sll.test.ts
+++ src/sll/sll.test.ts
@@ -42,4 +42,66 @@ describe("SLL", () => {
     expect(sll.root?.next?.value).toBe(2);
     expect(sll.root?.next?.next?.value).toBe(1);
   });
+
+  it("should get node at index", () => {
+    const sll = new SLL();
+    sll.push(1);
+    sll.push(2);
+    sll.push(3);
+    expect(sll.get(0)).toBe(1);
+    expect(sll.get(1)).toBe(2);
+    expect(sll.get(2)).toBe(3);
+  });
+
+  it("should set node at index", () => {
+    const sll = new SLL();
+    sll.push(1);
+    sll.push(2);
+    sll.push(3);
+    sll.set(0, 4);
+    sll.set(1, 5);
+    sll.set(2, 6);
+    expect(sll.root?.value).toBe(4);
+    expect(sll.root?.next?.value).toBe(5);
+    expect(sll.root?.next?.next?.value).toBe(6);
+  });
+
+  it("should insert node at index", () => {
+    const sll = new SLL();
+    sll.push(1);
+    sll.push(2);
+    sll.push(3);
+    sll.insert(0, 4);
+    sll.insert(1, 5);
+    sll.insert(2, 6);
+    expect(sll.root?.value).toBe(4);
+    expect(sll.root?.next?.value).toBe(5);
+    expect(sll.root?.next?.next?.value).toBe(6);
+    expect(sll.root?.next?.next?.next?.value).toBe(1);
+    expect(sll.root?.next?.next?.next?.next?.value).toBe(2);
+    expect(sll.root?.next?.next?.next?.next?.next?.value).toBe(3);
+  });
+
+  it("should remove node at index", () => {
+    const sll = new SLL();
+    sll.push(1);
+    sll.push(2);
+    sll.push(3);
+    sll.remove(0);
+    sll.remove(1);
+    sll.remove(2);
+    expect(sll.root?.value).toBe(2);
+    expect(sll.root?.next).toBeUndefined();
+  });
+
+  it("should find at index", () => {
+    const sll = new SLL();
+    sll.push(1);
+    sll.push(2);
+    sll.push(3);
+    expect(sll.find(1)).toBe(1);
+    expect(sll.find(2)).toBe(2);
+    expect(sll.find(3)).toBe(3);
+    expect(sll.find(4)).toBeUndefined();
+  });
 });
--- src/sll/sll.ts
+++ src/sll/sll.ts
@@ -27,7 +27,7 @@ export class SLL {
     current.next = node;
   }
 
-  pop() {
+  pop(): number | undefined {
     if (!this.root) {
       return;
     }
@@ -39,14 +39,18 @@ export class SLL {
       current = current.next;
     }
     prev.next = undefined;
+    return current.value;
   }
 
-  shift() {
+  shift(): number | undefined {
     if (!this.root) {
       return;
     }
 
+    const value = this.root.value;
     this.root = this.root.next;
+
+    return value;
   }
 
   unshift(value: number) {
@@ -61,7 +65,7 @@ export class SLL {
     this.root = node;
   }
 
-  get(index: number) {
+  private _getNode(index: number): SLLNode | undefined {
     if (!this.root) {
       return;
     }
@@ -75,10 +79,64 @@ export class SLL {
     return current;
   }
 
+  get(index: number): number | undefined {
+    const node = this._getNode(index);
+    return node ? node.value : undefined;
+  }
+
   set(index: number, value: number) {
-    const node = this.get(index);
+    if (!this.root) {
+      return;
+    }
+    const node = this._getNode(index);
     if (node) {
       node.value = value;
+    }
+  }
+
+  insert(index: number, value: number) {
+    if (index === 0) {
+      this.unshift(value);
+      return;
+    }
+
+    const prev = this._getNode(index - 1);
+    if (!prev) {
+      return;
+    }
+
+    const node = new SLLNode(value);
+    node.next = prev.next;
+    prev.next = node;
+  }
+
+  remove(index: number): number | undefined {
+    if (index === 0) {
+      return this.shift();
+    }
+
+    const prev = this._getNode(index - 1);
+    if (!prev || !prev.next) {
+      return;
+    }
+
+    const value = prev.next.value;
+    prev.next = prev.next.next;
+
+    return value;
+  }
+
+  find(value: number): number | undefined {
+    if (!this.root) {
+      return;
+    }
+
+    let current: SLLNode | undefined = this.root;
+    while (current) {
+      if (current.value === value) {
+        return current.value;
+      }
+      current = current.next;
     }
   }
 }
